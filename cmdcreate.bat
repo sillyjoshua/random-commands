9b1eae2e90a709e97da7bcbeacd72a8bed7b4914
sillyjoshua <joshuaisverycool2024@gmail.com>
2025-11-03 20:07:01 +0000

    Add files via upload

---
+++ cmdcreate.bat
@@ -0,0 +1,69 @@
+@echo off
+
+doskey info=echo This Currently Doesnt Have Any Information Available.
+
+doskey dad=python print ("dad i love you")
+
+doskey a=echo A
+
+doskey b=echo B
+
+doskey c=echo C
+
+doskey d=echo D
+
+doskey e=echo E
+
+doskey f=echo F
+
+doskey g=echo G
+
+doskey h=echo H
+
+doskey i=echo I
+
+doskey j=echo J
+
+doskey k=echo K
+
+doskey l=echo L
+
+doskey m=echo M
+
+doskey n=echo N
+
+doskey o=echo O
+
+doskey p=echo P
+
+doskey q=echo Q
+
+doskey r=echo R
+
+doskey s=echo S
+
+doskey t=echo T
+
+doskey u=echo U
+
+doskey v=echo V
+
+doskey w=echo W
+
+doskey x=echo X
+
+doskey y=echo Y
+
+doskey z=echo Z
+
+doskey emailme=start mailto:joshuaking@joshuakingstech.com
+
+doskey website=start https://www.joshuakingstech.com/
+
+doskey github=start https://www.github.com/sillyjoshua
+
+doskey youtube=start https://www.youtube.com/@joshuakingiscool
+
+doskey clear=cls
+
+doskey closecmd=exit
